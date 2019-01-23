09549de14ed587f06f746aff6732afed3b094c13
Imre Nagi <imre.nagi2812@gmail.com>
2019-01-23 19:29:45 +0700

    Change output format

--- src/main/java/com/imrenagi/banktrxanalytics/BatchPipeline.java
+++ src/main/java/com/imrenagi/banktrxanalytics/BatchPipeline.java
@@ -12,8 +12,13 @@ import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.transforms.windowing.IntervalWindow;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.joda.time.Instant;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Date;
 
 public class BatchPipeline {
 
@@ -30,7 +35,6 @@ public class BatchPipeline {
 		ignored. So only the window works.
 		 */
 		PCollection<KV<String, Long>> globalWindow = transfers.apply(new CalculateTransfer(Duration.standardHours(1), Duration.standardMinutes(10)));
-
 		globalWindow.apply(ParDo.of(new DoFn<KV<String, Long>, String>() {
 			@ProcessElement
 			public void processElement(ProcessContext context) {
@@ -39,6 +43,8 @@ public class BatchPipeline {
 				context.output(out);
 			}
 		})).apply(TextIO.write().to("output-global"));
+
+
 
 		transfers.apply(new CalculatePeriodicTransfer(Duration.standardMinutes(10),
 				Duration.standardDays(1),
@@ -49,7 +55,13 @@ public class BatchPipeline {
 					public void processElement(IntervalWindow window, ProcessContext context) {
 
 						KV<String, Long> kv = context.element();
-						String out = String.format("%s - %s -> %s:\t%d", window.start(), window.end(), kv.getKey(), kv.getValue());
+
+						DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy/MM/dd HH:mm:ss");
+
+						DateTime startDT = window.start().toDateTime();
+						DateTime endDT = window.end().toDateTime();
+
+						String out = String.format("%s - %s -> %s:\t%d", formatter.print(startDT), formatter.print(endDT), kv.getKey(), kv.getValue());
 						context.output(out);
 					}
 				})).apply(TextIO.write().to("output-periodic"));
